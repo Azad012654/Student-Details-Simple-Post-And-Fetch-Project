ea69e615839df466fab037f92885cd0d05c5da03
Azad012654 <azadchauhan012654@gmail.com>
2023-07-06 15:39:35 +0530

    Updated

--- src/main/java/com/student/details/studentdetails/controller/StudentController.java
+++ src/main/java/com/student/details/studentdetails/controller/StudentController.java
@@ -1,16 +1,18 @@
 package com.student.details.studentdetails.controller;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.details.studentdetails.Dao.StudentDao;
 import com.student.details.studentdetails.model.Student;
+
 
 
 
@@ -28,6 +30,17 @@ public class StudentController {
 		return new ResponseEntity<Student>(student, HttpStatus.OK);
 		}catch(Exception e) {
 			return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/get-student")
+	public ResponseEntity<?> getStudent(){
+			
+			List<Student> student = studentdao.findAll();
+			if(student.size()>0) {
+			return new ResponseEntity<List<Student>>(student, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>("No Data Found",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
